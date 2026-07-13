c121f166d6b30c1e4d65d8705be7db62bee95e4a
Tomasz <kwp@tlen.pl>
2026-07-13 10:34:31 +0000

    dodanie update task

--- app/routes.py
+++ app/routes.py
@@ -20,4 +20,9 @@ def create_task():
 
 @bp.route("/tasks/<int:task_id>", methods=["PATCH"])
 def update_task(task_id):
-    pass
+    update_data = request.json
+    for task in tasks:
+        if task["id"] == task_id:
+            task.update(update_data)
+            return jsonify(task), 200
+    return jsonify({"error": "Task not found"}), 404
